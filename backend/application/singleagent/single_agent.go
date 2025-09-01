31b379bfbe8ee4d5be452f27a1bb532cd8a7a13d
lijunwen.gigoo <lijunwen.gigoo@bytedance.com>
2025-09-01 10:33:26 +0800

    fix(singleagent): shortcmd without default value

--- backend/application/singleagent/single_agent.go
+++ backend/application/singleagent/single_agent.go
@@ -715,15 +715,18 @@ func (s *SingleAgentApplicationService) GetAgentOnlineInfo(ctx context.Context, 
 				AgentID:       ptr.Of(si.ObjectID),
 				Command:       si.ShortcutCommand,
 				Components: slices.Transform(si.Components, func(i *playground.Components) *bot_common.ShortcutCommandComponent {
-					return &bot_common.ShortcutCommandComponent{
+					sc := &bot_common.ShortcutCommandComponent{
 						Name:          i.Name,
 						Description:   i.Description,
 						Type:          i.InputType.String(),
 						ToolParameter: ptr.Of(i.Parameter),
 						Options:       i.Options,
-						DefaultValue:  ptr.Of(i.DefaultValue.Value),
 						IsHide:        i.Hide,
 					}
+					if i.DefaultValue != nil {
+						sc.DefaultValue = ptr.Of(i.DefaultValue.Value)
+					}
+					return sc
 				}),
 			}
 		})
